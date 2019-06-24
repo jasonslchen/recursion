10187eda19e9553a37e3e21ab4142a19e9062e9e
Jason Chen <jason.sl.chen@gmail.com>
2019-06-24 11:04:13 -0700

    first attempt at JSONstringify recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,25 @@
 
 // but you don't so you're going to write it from scratch:
 
+//Array = final stringified object array 
+//
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let stringify = [];
+  let values = Object.keys(obj);
+  if (values.length === 0) {
+    return '{' + stringify.join(', ') + '}';
+  }
+  stringify.push(String(values[0]) + ':' + String(obj[values[0]]));
+  delete obj[(values[0])];
+  return stringifyJSON(obj);
 };
+
+let x = {
+  y : 5,
+  jason : 4,
+  bob : 'jason'
+}
+
+console.log(stringifyJSON(x));
+
