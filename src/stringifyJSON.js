1540eb512d756759745fac5af16ea6ba0911f825
Jason Chen <jason.sl.chen@gmail.com>
2019-06-24 14:38:05 -0700

    submit stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,21 +7,37 @@
 //
 
 var stringifyJSON = function(obj) {
-  let stringify = [];
-  let values = Object.keys(obj);
-  if (values.length === 0) {
-    return '{' + stringify.join(', ') + '}';
+  let arrayString = [];
+  let objString = [];
+  //check primitive type
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === undefined) {
+    return undefined;
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+    for (let i = 0; i < obj.length; i++) {
+      arrayString.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arrayString + ']';
+    }
+  } else if (obj instanceof Object) {
+    let objKeys = Object.keys(obj);
+    for (let i = 0; i < objKeys.length; i++) {
+      let key = '"' + objKeys[i] + '":';
+      let values = obj[objKeys[i]];
+      if (typeof values === 'number' || typeof values === 'string' || typeof values === 'boolean' || values === null) {
+        objString.push(key + stringifyJSON(values));
+      } else if (values instanceof Function || typeof values === undefined || typeof values === 'symbol') {
+        objString.push('');
+      } else if (values instanceof Object) {
+        objString.push(key + stringifyJSON(values));
+      }  
+    }
+    return '{' + objString + '}';
   }
-  stringify.push(String(values[0]) + ':' + String(obj[values[0]]));
-  delete obj[(values[0])];
-  return stringifyJSON(obj);
 };
-
-let x = {
-  y : 5,
-  jason : 4,
-  bob : 'jason'
-}
-
-console.log(stringifyJSON(x));
-
